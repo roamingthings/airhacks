529cb95a672e7dff0fb378fa2226a4e0417e900d
Adam Bien <abien@adam-bien.com>
2016-06-13 11:03:18 +0200

    Ambiguous DI leads to Ambiguous dependencies error

---
+++ basics-on-server/src/main/java/com/airhacks/AnotherFactory.java
@@ -0,0 +1,13 @@
+package com.airhacks;
+
+import javax.enterprise.inject.Produces;
+
+/**
+ *
+ * @author airhacks.com
+ */
+public class AnotherFactory {
+
+    @Produces
+    private String another = "who wins";
+}
