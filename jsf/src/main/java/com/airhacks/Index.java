48cda2283c34ee84d58a87752b31aa1ea22c723d
Adam Bien <abien@adam-bien.com>
2016-06-13 16:48:12 +0200

    Validator can be injected for "manual" validation

--- jsf/src/main/java/com/airhacks/Index.java
+++ jsf/src/main/java/com/airhacks/Index.java
@@ -1,6 +1,10 @@
 package com.airhacks;
 
+import java.util.Set;
 import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
 import javax.validation.constraints.Size;
 
 /**
@@ -13,6 +17,9 @@ public class Index {
     @Size(min = 2, max = 5)
     private String message;
 
+    @Inject
+    Validator validator;
+
     public String getMessage() {
         return message;
     }
@@ -23,6 +30,7 @@ public class Index {
 
     public Object save() {
         System.out.println("--- " + message);
+        Set<ConstraintViolation<Index>> violations = validator.validate(this);
         return null;
     }
 
