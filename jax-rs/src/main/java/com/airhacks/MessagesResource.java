ef24aeea76a02cd570f4b2c2c120be3ffedd4593
Adam Bien <abien@adam-bien.com>
2016-06-13 15:40:49 +0200

    Location header is working

--- jax-rs/src/main/java/com/airhacks/MessagesResource.java
+++ jax-rs/src/main/java/com/airhacks/MessagesResource.java
@@ -6,6 +6,7 @@ import javax.json.JsonObject;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
@@ -29,6 +30,15 @@ public class MessagesResource {
                 path("/" + System.currentTimeMillis()).
                 build();
         return Response.created(uri).build();
+    }
+
+    @GET
+    @Path("{id}")
+    public JsonObject find(@PathParam("id") long id) {
+        return Json.createObjectBuilder().
+                add("name", "duke " + id).
+                add("id", id).
+                build();
     }
 
 }
