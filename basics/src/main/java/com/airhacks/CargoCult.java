cf4136db817c6a23cd1e381fec67868bc464f6bd
Adam Bien <abien@adam-bien.com>
2016-06-13 09:46:40 +0200

    Basic annotation explained

---
+++ basics/src/main/java/com/airhacks/CargoCult.java
@@ -0,0 +1,19 @@
+package com.airhacks;
+
+/**
+ *
+ * @author airhacks.com
+ */
+public class CargoCult {
+
+    @NoMagic
+    String message;
+
+    String anotherNotAnnotated;
+
+    @NoMagic
+    public void doSomething() {
+
+    }
+
+}
---
+++ basics/src/test/java/com/airhacks/CargoCultTest.java
@@ -0,0 +1,25 @@
+package com.airhacks;
+
+import java.lang.reflect.Field;
+import org.junit.Test;
+
+/**
+ *
+ * @author airhacks.com
+ */
+public class CargoCultTest {
+
+    @Test
+    public void findAnnotation() {
+        Field[] declaredFields = CargoCult.class.getDeclaredFields();
+        for (Field declaredField : declaredFields) {
+            NoMagic annotation = declaredField.getAnnotation(NoMagic.class);
+            if (annotation != null) {
+                System.out.println("annotation = " + annotation + " on field " + declaredField);
+            } else {
+                System.out.println("declaredField = " + declaredField + " is not annotated");
+            }
+        }
+    }
+
+}
